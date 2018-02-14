1f4113b6d8765f15cd59cb02c03508e9f4b740fe
Bruno Tomé <ibrunotome@gmail.com>
2018-02-14 09:30:56 -0200

    Lidando com colecões - v-for

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import {Time} from './time';
 new Vue({
   el: '#app',
   data: {
-    time: new Time("América MG", require('./assets/america_mg_60x60.png'))
+    times: [
+      new Time("América MG", require('./assets/america_mg_60x60.png')),
+      new Time("Atlético MG", require('./assets/atletico_mg_60x60.png')),
+      new Time("Cruzeiro", require('./assets/cruzeiro_60x60.png')),
+    ],
+    alfabeto: {
+      a: 'A',
+      b: 'B',
+      c: 'C',
+      d: 'D',
+      e: 'E',
+    }
   }
 })
